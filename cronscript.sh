905ff1c08474397fe8bfaa55a90be93353c65d5c
Max Klingelhoeffer <makl11@users.noreply.github.com>
2020-12-26 18:33:15 +0100

    fix: update cronscript for new container structure

--- cronscript.sh
+++ cronscript.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 NODE_PATH=$(which node)
 NTBA_FIX_319=1
-sh -c "${NODE_PATH} /home/node/app/index.js" >> /var/log/corn_notify.log
+sh -c "${NODE_PATH} /app/dist/index.js" >> /var/log/corn_notify.log
